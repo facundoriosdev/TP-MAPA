9cf97e43190248601d35af8b5aec0f363a248fbf
Emiliano Acosta <emilianoacosta057@gmail.com>
2026-05-12 12:35:39 -0300

    Se agrego imagen de fondo para las pantallas

--- src/view2/Menu.java
+++ src/view2/Menu.java
@@ -10,10 +10,14 @@ import javax.swing.SwingConstants;
 import view.presenter.MenuPresenter;
 
 import java.awt.Font;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.Color;
 
 public class Menu {
 
@@ -38,20 +42,31 @@ public class Menu {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		Icon backgroundIcon = new ImageIcon(getClass().getResource("/imagenes/fondo.jpg"));
+		JLabel backgroundLabel = new JLabel(backgroundIcon);
+		backgroundLabel.setBounds(-175, 0, 772, 531);
+		
 		JLabel lblTitulo = new JLabel("Complete los datos");
-		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblTitulo.setForeground(new Color(255, 255, 255));
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblTitulo.setBounds(143, 21, 200, 27);
 		frame.getContentPane().add(lblTitulo);
 		
 		JLabel lblCosto = new JLabel("Costo por KM");
+		lblCosto.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblCosto.setForeground(new Color(255, 255, 255));
 		lblCosto.setBounds(10, 73, 200, 14);
 		frame.getContentPane().add(lblCosto);
 		
 		JLabel lblAumento = new JLabel("Porcentaje de aumento");
+		lblAumento.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblAumento.setForeground(new Color(255, 255, 255));
 		lblAumento.setBounds(10, 125, 200, 14);
 		frame.getContentPane().add(lblAumento);
 		
 		JLabel lblFijo = new JLabel("Costo fijo interprovincial");
+		lblFijo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblFijo.setForeground(new Color(255, 255, 255));
 		lblFijo.setBounds(10, 170, 200, 38);
 		frame.getContentPane().add(lblFijo);
 		
@@ -80,9 +95,12 @@ public class Menu {
 		txtCostoInterprovincial.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("(Pasando los 300KM)");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
 		lblNewLabel_4.setBounds(10, 145, 123, 14);
 		frame.getContentPane().add(lblNewLabel_4);
-
+		
+		frame.getContentPane().add(backgroundLabel);
 	}
 
 	public String getCostoKm() { return txtCostoKm.getText(); }
